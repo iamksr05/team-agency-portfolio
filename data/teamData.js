0a20c06a92c40ee84d228cffbd62be740c993ef1
Priyanshu Sharma <f26wpjsvqv@privaterelay.appleid.com>
2026-06-16 12:29:56 +0530

    team data file added to data folder

---
+++ data/teamData.js
@@ -0,0 +1,37 @@
+const teamMembers = [
+    {
+        name: "Jayati Sharma",
+        role: "Role 1",
+        bio: "Personal Description here.....",
+        image: "assets/images/member1.jpg",
+        github: "https://github.com/jayatisharma15j-oss"
+    },
+    {
+        name: "Karan Ram",
+        role: "Role 2",
+        bio: "Personal Description here.....",
+        image: "assets/images/member2.jpg",
+        github: "#"
+    },
+    {
+        name: "Lakshya Saxena",
+        role: "Role 3",
+        bio: "Personal Description here.....",
+        image: "assets/images/member3.jpg",
+        github: "#"
+    },
+    {
+        name: "Piyush Kumar Bharti",
+        role: "Role 4",
+        bio: "Personal Description here.....",
+        image: "assets/images/member4.jpg",
+        github: "#"
+    },
+    {
+        name: "Priyanshu Sharma",
+        role: "Role 5",
+        bio: "Personal Description here.....",
+        image: "assets/images/member5.jpg",
+        github: "#"
+    }
+];
